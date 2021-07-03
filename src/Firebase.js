6a64eb2c2d4e9f65188ab84445c59459e04a9bb7
Sishir Giri <sis1001@berkeley.edu>
2021-07-03 11:33:44 -0700

    restructued file

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,16 @@
 import firebase from "firebase";
 
-//const PROJ_STATUS = "development";
+// const PROJ_STATUS = "production";
 
-const fbDevConfig = {
-  apiKey: process.env.REACT_APP_FB_DEV_API_KEY,
-  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN_DEV,
-  projectId: process.env.REACT_APP_FB_PROJECT_ID_DEV,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_DEV,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_DEV,
-  appId: process.env.REACT_APP_APP_ID_DEV,
+const fbProductionConfig = {
+  apiKey: process.env.REACT_APP_FB_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN_PROD,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID_PROD,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_PROD,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_PROD,
+  appId: process.env.REACT_APP_APP_ID_PROD,
 };
 
-firebase.initializeApp(fbDevConfig);
+firebase.initializeApp(fbProductionConfig);
 
 export default firebase;
