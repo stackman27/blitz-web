183bcd05bf63744bff3b395268ab51e21b75ad9f
Sishir Giri <sis1001@berkeley.edu>
2021-07-17 12:19:27 -0700

    test

--- src/constants/Variables.js
+++ src/constants/Variables.js
@@ -1,2 +1,3 @@
-const getUser = JSON.parse(localStorage.getItem("user"));
+// gets the current logged in user
+const getUser = JSON.parse(localStorage.getItem('user'));
 export const vendorUid = getUser?.uid;
