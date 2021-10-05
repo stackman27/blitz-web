f238fba8d893410c80e97787743c2e73aa46a84e
stackman27 <sis1001@berkeley.edu>
2021-10-04 23:07:17 -0700

    fixed discount total issue

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -19,14 +19,16 @@ function HomeBody() {
       currentUser.uid,
       getTags,
     ).onSnapshot((snap) => {
-      snap.forEach((doc) => {
-        if (doc.data()) {
-          setScannedReceipt(true);
-          setPurchaseInfo(doc.data());
-          setTimeout(() => {
-            setScannedReceipt(false);
-          }, 30000);
-        }
+      snap.docChanges().forEach(() => {
+        snap.forEach((doc) => {
+          if (doc.data()) {
+            setScannedReceipt(true);
+            setPurchaseInfo(doc.data());
+            setTimeout(() => {
+              setScannedReceipt(false);
+            }, 30000);
+          }
+        });
       });
     });
     return () => unsubscribe();
